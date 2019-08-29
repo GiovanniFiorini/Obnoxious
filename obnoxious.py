0f7193974d5e10340e8a8e16465bd1b16e6e4399
Giovanni <gio.fiore.94@gmail.com>
2019-08-29 16:35:03 +0200

    Variabe Depth Search Algorithm: * Implemented the variable depth search heuristic * Renamed local_search in large_neighborhood_search * Refactored main file in a multi-choice function to handle the decision of the algorithm * Enhanchments of the code readability * Minor fixes and enhanchments

--- obnoxious.py
+++ obnoxious.py
@@ -1,16 +1,31 @@
+import time
 from commons.setup_vars import setup
 from heuristics.mhf_greedy import mhf_greedy
 from heuristics.mchrf_greedy import mchrf_greedy
-from heuristics.local_search import large_neighborhood_search
+from heuristics.large_neighborhood_search import large_neighborhood_search
+from heuristics.variable_depth_search import variable_depth_search
 from commons.utils import show_facility_usage_by_town, show_greedy_result, show_local_search_result
-import time
 
 
-def obnoxious(choice: int, filename: str):
+def obnoxious(greedy: int, local_search: int, filename: str):
+    """
+    The main function, it runs the algorithm chosen
+
+    :param greedy: the greedy chosen
+    :param local_search: the local search chosen
+    :param filename: the name of the file containing the instance
+    :return: None
+    """
+
     facilities, towns, hazards = setup(f"./instances/{filename}.json")
 
-    if choice == 1:
+    start = 0
+    end = 0
 
+    iterations = 0
+    improvements = 0
+
+    if greedy == 1:
         start = time.time()
         greedy_success = mhf_greedy(facilities, towns)
         end = time.time()
@@ -19,19 +34,7 @@ def obnoxious(choice: int, filename: str):
             print("The Minimum Hazard Facility Greedy failed.")
             return
 
-        duration = end - start
-
-        show_greedy_result(facilities, towns, hazards, duration)
-
-        start = time.time()
-        iterations, improvements = large_neighborhood_search(facilities, towns, hazards, 50, 10)
-        end = time.time()
-
-        duration = end - start
-
-        show_local_search_result(facilities, towns, hazards, duration, iterations, improvements)
-
-    if choice == 2:
+    if greedy == 2:
 
         start = time.time()
         greedy_success = mchrf_greedy(facilities, towns)
@@ -41,16 +44,24 @@ def obnoxious(choice: int, filename: str):
             print("The Minimum Capacity/Hazard Ratio Facility Greedy failed.")
             return
 
-        duration = end - start
+    duration = end - start
 
-        show_greedy_result(facilities, towns, hazards, duration)
+    show_greedy_result(facilities, towns, hazards, duration)
+
+    if local_search == 1:
 
         start = time.time()
         iterations, improvements = large_neighborhood_search(facilities, towns, hazards, 50, 10)
         end = time.time()
 
-        duration = end - start
+    if local_search == 2:
 
-        show_local_search_result(facilities, towns, hazards, duration, iterations, improvements)
+        start = time.time()
+        iterations, improvements = variable_depth_search(facilities, towns, hazards, 50, 10)
+        end = time.time()
 
-    show_facility_usage_by_town(towns)
+    duration = end - start
+
+    show_local_search_result(facilities, towns, hazards, duration, iterations, improvements)
+
+    # show_facility_usage_by_town(towns)
